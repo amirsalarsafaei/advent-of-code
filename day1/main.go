e3364eef743b5ff6ddf7be8f81c1b74c4a8eef36
AmirSalar Safaei <amirsalar.safaei@divar.ir>
2023-08-12 22:19:50 +0330

    solved day 1

---
+++ day1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+func main() {
+	fileBytes, err := os.ReadFile("./day1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	inputString := string(fileBytes)
+
+	elvesBags := strings.Split(inputString, "\n\n")
+
+	maxCal := lo.Max(lo.Map(elvesBags, func(bag string, _ int) int64 {
+		return getElfCalories(bag)
+	}))
+
+	fmt.Print(maxCal)
+}
+
+func getElfCalories(elfBag string) int64 {
+	var sum int64
+
+	for _, calStr := range strings.Fields(elfBag) {
+		cal, _ := strconv.ParseInt(calStr, 10, 64)
+		sum += cal
+	}
+
+	return sum
+}
---
+++ day1/part2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+func main() {
+	fileBytes, err := os.ReadFile("./day1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	inputString := string(fileBytes)
+
+	elvesBags := strings.Split(inputString, "\n\n")
+
+	elvesCals := lo.Map(elvesBags, func(bag string, _ int) int {
+		return getElfCalories(bag)
+	})
+
+	sort.Ints(elvesCals)
+
+	var sum int
+
+	for i := 1; i <= 3; i++ {
+		sum += elvesCals[len(elvesCals)-i]
+	}
+
+	fmt.Print(sum)
+}
+
+func getElfCalories(elfBag string) int {
+	var sum int
+
+	for _, calStr := range strings.Fields(elfBag) {
+		cal, _ := strconv.Atoi(calStr)
+		sum += cal
+	}
+
+	return sum
+}
