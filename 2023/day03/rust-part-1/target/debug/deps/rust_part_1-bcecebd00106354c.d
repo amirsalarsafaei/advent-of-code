78fec51ae14a50145b6a51f546085edec72c5daa
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2023-12-04 19:25:03 +0330

    cleaned

--- 2023/day03/rust-part-1/target/debug/deps/rust_part_1-bcecebd00106354c.d
+++
@@ -1,6 +0,0 @@
-/Users/divar/advent-of-code/2023/day03/rust-part-1/target/debug/deps/rust_part_1-bcecebd00106354c: src/main.rs src/../input.txt
-
-/Users/divar/advent-of-code/2023/day03/rust-part-1/target/debug/deps/rust_part_1-bcecebd00106354c.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
