04c931cf9c9b9321e63e58a1581c8747861367f3
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2025-12-21 14:58:48 +0330

    [2025] day2 part2

--- 2025/day02/src/bin/part2.rs
+++ 2025/day02/src/bin/part2.rs
@@ -7,13 +7,19 @@ fn main() {
             for i in *start..=*end {
                 let s = i.to_string();
 
-                let chars: Vec<char> = s.chars().collect();
-
-                
-
                 for chunk in 1..s.len() {
                     if s.len() % chunk != 0 {
                         continue
+                    }
+                    let chuck_cnt = s.len() / chunk;
+                    let is_valid = (0..chuck_cnt).fold(true, |acc, n| {
+                        let part1 = &s[n*chunk..(n+1)*chunk];
+                        let part2 = &s[0..chunk];
+                        acc && part1 == part2
+                    });
+                    if is_valid {
+                        sum += i;
+                        break;
                     }
                 }
 
