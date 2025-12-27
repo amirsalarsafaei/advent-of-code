15a36844d3661786615a14628af7ef4f123a57df
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2025-12-27 11:40:25 +0330

    fixed day09 part2

--- 2025/day09/src/bin/part2.rs
+++ 2025/day09/src/bin/part2.rs
@@ -4,53 +4,48 @@ use day09::{get_tiles, InputType};
 use itertools::Itertools;
 
 fn main() {
-    let tiles = get_tiles(InputType::Sample).expect("failed to get tiles");
+    let tiles = get_tiles(InputType::Test).expect("failed to get tiles");
 
     let res = (0..tiles.len())
         .tuple_combinations()
         .fold(0, |f_max, (i, j)| {
             let tile_i = tiles[i];
             let tile_j = tiles[j];
-            let horiz_lines = [
-                (tile_i.x, (tile_i.y, tile_j.y)),
-                (tile_j.x, (tile_i.y, tile_j.y)),
-            ];
-            let vert_lines = [
-                (tile_i.y, (tile_i.x, tile_j.x)),
-                (tile_j.y, (tile_i.x, tile_j.x)),
-            ];
+            let min_rect_x = min(tile_i.x, tile_j.x);
+            let max_rect_x = max(tile_i.x, tile_j.x);
+            let min_rect_y = min(tile_i.y, tile_j.y);
+            let max_rect_y = max(tile_i.y, tile_j.y);
 
             let mut is_valid = true;
             (0..tiles.len())
                 .chain(std::iter::once(0))
                 .tuple_windows()
-                .for_each(|(k, x)| {
+                .for_each(|(k, o)| {
                     let tile_k = tiles[k];
-                    let tile_x = tiles[x];
-                    for (x, (y1, y2)) in horiz_lines {
-                        if x > min(tile_k.x, tile_x.x)
-                            && x < max(tile_k.x, tile_x.x)
-                            && min(max(y1, y2), max(tile_k.y, tile_x.y))
-                                - max(min(y1, y1), min(tile_k.y, tile_x.y))
-                                >= 0
+                    let tile_o = tiles[o];
+
+                    if tile_o.x == tile_k.x {
+                        let x = tile_o.x;
+                        let min_y = min(tile_o.y, tile_k.y);
+                        let max_y = max(tile_o.y, tile_k.y);
+                        if x > min_rect_x && x < max_rect_x && 
+                            min(max_y, max_rect_y) - max(min_y, min_rect_y) >= 0
                         {
                             is_valid = false;
-                            break;
+                            return;
                         }
-                    }
-                    for (y, (x1, x2)) in vert_lines {
-                        if y > min(tile_k.y, tile_x.y)
-                            && y < max(tile_k.y, tile_x.y)
-                            && min(max(x1, x2), max(tile_k.x, tile_x.x))
-                                - max(min(x1, x2), min(tile_k.x, tile_x.x))
-                                >= 0
+                    } else {
+                        let y = tile_o.y;
+                        let min_x = min(tile_o.x, tile_k.x);
+                        let max_x = max(tile_o.x, tile_k.x);
+                        if y > min_rect_y && y < max_rect_y && 
+                            min(max_x, max_rect_x) - max(min_x, min_rect_x) >= 0
                         {
                             is_valid = false;
-                            break;
+                            return;
                         }
                     }
                 });
-
             if is_valid {
                 max(
                     f_max,
